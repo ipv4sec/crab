74231e921147754dc9ab37b349e9353573f23815
Loli <ipv4sec@gmail.com>
2021-11-05 15:55:48 +0800

    fix: api app/status

--- status/handler.go
+++ status/handler.go
@@ -1,48 +1,45 @@
 package status
 
 import (
-	"crab/aam/v1alpha1"
-	"crab/app"
 	"crab/db"
 	"crab/utils"
 	"github.com/gin-gonic/gin"
-	"gopkg.in/yaml.v3"
 	"k8s.io/klog/v2"
 	"strconv"
 )
 
 func GetStatusHandlerFunc(c *gin.Context) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(200, utils.ErrorResponse(utils.ErrBadRequest, "参数错误"))
-		return
-	}
-	var val app.App
-	err := db.Client.Where("id = ?", id).Find(&val).Error
-	if err != nil {
-		klog.Errorln("数据库查询错误:", err.Error())
-		c.JSON(200, utils.ErrorResponse(utils.ErrDatabaseBadRequest, "该实例不存在"))
-		return
-	}
-	var manifest v1alpha1.Application
-	err = yaml.Unmarshal([]byte(val.Manifest), &manifest)
-	if err != nil {
-		klog.Errorln("解析描述文件错误:", err.Error())
-		c.JSON(200, utils.ErrorResponse(utils.ErrInternalServer, "解析描述文件错误"))
-		return
-	}
-
-	var status []Status
-	err = db.Client.Where("id = ?", id).Find(&status).Error
-	if err != nil {
-		klog.Errorln("数据库查询错误:", err.Error())
-		c.JSON(200, utils.ErrorResponse(utils.ErrDatabaseInternalServer, "数据库查询错误"))
-		return
-	}
-	if len(manifest.Spec.Workloads) == len(status) {
-		c.JSON(200, utils.SuccessResponse(1))
-		return
-	}
+	//id := c.Param("id")
+	//if id == "" {
+	//	c.JSON(200, utils.ErrorResponse(utils.ErrBadRequest, "参数错误"))
+	//	return
+	//}
+	//var val app.App
+	//err := db.Client.Where("id = ?", id).Find(&val).Error
+	//if err != nil {
+	//	klog.Errorln("数据库查询错误:", err.Error())
+	//	c.JSON(200, utils.ErrorResponse(utils.ErrDatabaseBadRequest, "该实例不存在"))
+	//	return
+	//}
+	//var manifest v1alpha1.Application
+	//err = yaml.Unmarshal([]byte(val.Manifest), &manifest)
+	//if err != nil {
+	//	klog.Errorln("解析描述文件错误:", err.Error())
+	//	c.JSON(200, utils.ErrorResponse(utils.ErrInternalServer, "解析描述文件错误"))
+	//	return
+	//}
+	//
+	//var status []Status
+	//err = db.Client.Where("id = ?", id).Find(&status).Error
+	//if err != nil {
+	//	klog.Errorln("数据库查询错误:", err.Error())
+	//	c.JSON(200, utils.ErrorResponse(utils.ErrDatabaseInternalServer, "数据库查询错误"))
+	//	return
+	//}
+	//if len(manifest.Spec.Workloads) == len(status) {
+	//	c.JSON(200, utils.SuccessResponse(1))
+	//	return
+	//}
 	c.JSON(200, utils.SuccessResponse(0))
 }
 
